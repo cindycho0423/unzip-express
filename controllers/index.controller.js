4be4635ecc656223286910c275d7ad22be58cf03
cindy Cho <cindy.cho@inhuman-z.com>
2025-05-20 15:04:19 +0900

    Add yauzl for zip file extraction and update routes and controllers
    - Integrated yauzl to handle zip file extraction in the unZipFile function.
    - Updated index.route.js to use the new unZipFile controller for file uploads.
    - Added yauzl as a dependency in package.json and package-lock.json.
    - Removed test1.gz and test2.gz files from the data directory.

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -18,3 +18,42 @@
 // 2. 서버사이드에서 압축파일을 정적 폴더에 저장 -> y
 // 3. 저장된 압축파일을 읽은 후 정적 파일 폴더에 압축 풀기
 // 4. 압축풀기가 완료되면 클라이언트에 성공 메시지 보내기
+
+const yauzl = require("yauzl");
+const path = require("path");
+const fs = require("fs");
+
+function unZipFile(req, res, next) {
+  if (!path.join(__dirname, "../uploads")) {
+    fs.mkdirSync(__dirname, "../uploads", { recursive: true });
+  }
+  const zipFilePath = path.join(__dirname, "../uploads", req.file.filename);
+  const extractPath = path.join(__dirname, "../extracted");
+
+  yauzl.open(zipFilePath, { lazyEntries: true }, function (err, zipfile) {
+    zipfile.readEntry();
+    zipfile.on("entry", function (entry) {
+      const filePath = path.join(extractPath, entry.fileName);
+
+      if (/\/$/.test(entry.fileName)) {
+        fs.mkdirSync(filePath, { recursive: true });
+        zipfile.readEntry();
+      } else {
+        zipfile.openReadStream(entry, function (err, readStream) {
+          const writeStream = fs.createWriteStream(filePath);
+          readStream.on("end", function () {
+            zipfile.readEntry();
+          });
+          readStream.pipe(writeStream);
+        });
+      }
+    });
+
+    zipfile.on("end", function () {
+      res.json({ success: true });
+    });
+  });
+}
+
+module.exports = { unZipFile };
+
--- routes/index.route.js
+++ routes/index.route.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const multer  = require('multer');
+const fs = require("fs");
+const { unZipFile } = require("../controllers/index.controller");
+
+let dir = "./uploads/";
+if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
 const upload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-    cb(null, "uploads/")
+      cb(null, "uploads/");
     },
     filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname)
+      cb(null, Date.now() + "-" + file.originalname);
     },
-  })
+  }),
 });
 
 // GET Home page
-router.get('/', (req, res) => {
-  res.render('index')
-})
+router.get("/", (req, res) => {
+  res.render("index");
+});
 
 // POST
-router.post('/', upload.single('file'), function(req, res, next){
-const { data } = req.body;
-console.log(req.file)
-  res.json({
-    success: true,
-  })
-})
+router.post("/", upload.single("file"), unZipFile);
 module.exports = router;
 
